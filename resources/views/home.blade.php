9dbdeaa7b6cd633146ee6406328be4a98e685af8
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-10 18:03:10 +0200

    Aggiunto main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,46 @@
 
       <main>
         
+        <div class="containerMain">
+          <h2>Le Lunghe</h2>
+
+          <div class="containerPasta">
+            
+              @foreach ($lunghe as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+                </div>
+              @endforeach
+            
+          </div>
+        
+          <h2>Le Corte</h2>
+
+          <div class="containerPasta">
+            
+              @foreach ($corte as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+                </div>
+              @endforeach
+            
+          </div>
+        
+          <h2>Le Cortissime</h2>
+
+          <div class="containerPasta">
+            
+              @foreach ($cortissime as $pasta)
+                <div class="card">
+                  <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+                </div>
+              @endforeach
+            
+          </div>
+        </div>
+
+        
+
       </main>
 
       <footer>
