79b65e9a83991a37756f4843c66b9280c32ab9c2
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-10 17:21:38 +0200

    Aggiunto header e footer

---
+++ resources/views/home.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Home La Molisana</title>
+  </head>
+  <body>
+    
+      <header>
+        <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo La Molisana">
+        <ul>
+          <li>Home</li>
+          <li>Prodotti</li>
+          <li>News</li>
+        </ul>
+      </header>
+
+      <main>
+        
+      </main>
+
+      <footer>
+        
+        <div class="container-footer">
+          <div class="colonna">
+            <img src="{{ asset('images/marchio-sito-test.png') }}" alt="Logo La Molisana">
+            <ul>
+              <li><strong>regione sociale:</strong> la molisana S.P.A.</li>
+              <li><strong>sede legale:</strong> contrada colle delle api, 100/a - 86100 - campobasso (cb)</li>
+              <li><strong>pec:</strong> lamolisana@pec.it</li>
+              <li><strong>tel:</strong> +39 0874 4981</li>
+              <li><strong>fax:</strong> +39 0874 629584</li>
+              <li>per segnalazioni scrive a: <strong><u>info@lamolisana.it</u></strong></li>
+              <li>per contattare l'area commerciale: <strong><u>commerciale@lamolisana.it</u></strong></li>
+              <li>per contattare l'area export: <strong><u>export@lamolisana.it</u></strong></li>
+              <li>numero verde 800818081</li>
+              <li>telefono 3801292455</li>
+            </ul>
+          </div>
+          <div class="colonna">
+            <ul class="lista-menu">
+              <li class="first">pastificio</li>
+              <li>Il Pastificio</li>
+              <li>Grano decorticato a pietra</li>
+              <li>Il Molise c'è</li>
+              <li>Filiera Integrata</li>
+              <li>100 anni di pasta</li>
+              <li>Sartoria della pasta</li>
+              <li>Spaghetto Quadrato</li>
+              <li>Le Persone</li>
+            </ul>
+            <ul class="lista-menu">
+              <li class="first">i prodotti</li>
+              <li>Le Classiche</li>
+              <li>Le Integrali</li>
+              <li>Le Speciali</li>
+              <li>Le Biologiche</li>
+              <li>Le Gluten-Free</li>
+              <li>Le Semole</li>
+              <li>Le Extra di Lusso</li>
+            </ul>
+          </div>
+          <div class="colonna">
+            <ul class="lista-menu">
+              <li class="first">collezione da chef</li>
+              <li>Collezione da Chef</li>
+              <li>Grandi Cucine</li>
+              <li>Biologiche</li>
+              <li>Quadrate</li>
+            </ul>
+          </div>
+        </div>
+        
+        <img src="{{ asset('images/footer-montagne-bottom.jpg') }}" alt="Montagne Footer Bottom">
+
+      </footer>
+
+  </body>
+</html>
